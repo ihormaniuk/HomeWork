858b8950e85562a7f1b64dc019abc90488ad2404
ihormaniuk <ihor.maniuk@gmail.com>
2020-03-26 14:23:11 +0200

    master

---
+++ src/LessonsJavaCore_12/Task_3/Deputy.java
@@ -0,0 +1,118 @@
+package LessonsJavaCore_12.Task_3;
+
+import java.util.Scanner;
+
+public class Deputy extends Human implements Comparable <Deputy> {
+
+    private String surname;
+    private String name;
+    private int age;
+    private boolean bribery;
+    private int bribeSize;
+
+
+    public Deputy(String surname,String name,int age,boolean bribery,int weight,int height){
+        super(weight,height);
+        this.surname = surname;
+        this.name = name;
+        this.age = age;
+        this.bribery = bribery;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isBribery() {
+        return bribery;
+    }
+
+    public void setBribery(boolean bribery) {
+        this.bribery = bribery;
+    }
+
+    public int getBribeSize() {
+        return bribeSize;
+    }
+
+    public void setBribeSize(int bribeSize) {
+        this.bribeSize = bribeSize;
+    }
+
+    public String toBribeToString(){
+        if(this.bribery){
+            return "Хабарник";
+        }else {
+            return "Чесна Людина";
+        }
+    }
+
+    public void toBribe() {
+        if (this.bribery == false) {
+            System.out.println("Цей депутат не бере хабарів");
+        }
+
+        if (this.bribery == true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(" Ведіть суму хабара ");
+            int bribeAmount = scanner.nextInt();
+
+            if (bribeAmount > 5000) {
+                System.out.println("Миліція увязнить депутата");
+            } else {
+                this.bribeSize = bribeAmount;
+            }
+        }
+    }
+
+//    public void toBribe(){
+//        Scanner scanner = new Scanner(System.in);
+//        int size = scanner.nextInt();
+//        if(bribery=false){
+//            print("Цей депутат не бере хабарів");
+//        }else if(size<=5000){
+//            print("Цей депутат взяв хабара на суму : " + size);
+//            size = bribeSize;
+//        }else if(size>=5000){
+//            print("Миліція увязнить депутата");
+//        }
+//    }
+
+    public String toString(){
+        return " Фамілія : " + surname + "," + " Імя : " + name + "\n" +
+                " Вік : " + age + " , " + " Вага : " + super.getWeight() + " , " + " Ріст : " + super.getHeight() + "\n" +
+                toBribeToString();
+    }
+
+    @Override
+    public int compareTo(Deputy o) {
+        if(this.getBribeSize() == o.getBribeSize()){
+            return 0;
+        }else if(this.getBribeSize() < o.getBribeSize()){
+            return -1;
+        }else {
+            return 1;
+        }
+    }
+}
---
+++ src/LessonsJavaCore_12/Task_3/Faction.java
@@ -0,0 +1,106 @@
+package LessonsJavaCore_12.Task_3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import static MyClassesToWork.Print.print;
+
+public class Faction {
+
+    private String nameFaction;
+
+    public Faction(String nameFaction){
+        this.nameFaction = nameFaction;
+    }
+
+    public String getNameFaction() {
+        return nameFaction;
+    }
+
+    public void setNameFaction(String nameFaction) {
+        this.nameFaction = nameFaction;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    List<Deputy> deputyList = new ArrayList<>();
+
+    public void addDeputy(){
+        print("Ведіть фамілію депутата ");
+        String surname = scanner.next();
+        print("Ведіть імя депутата");
+        String name = scanner.next();
+        print("Скільки років депутату ?");
+        int age = scanner.nextInt();
+        print("Ведіть вагу депутата");
+        int weight = scanner.nextInt();
+        print("Ведіть ріст депутата");
+        int height = scanner.nextInt();
+        print("Депутат бере хабар ? (true/false)");
+        boolean bribery = scanner.nextBoolean();
+        Deputy deputy = new Deputy(surname,name,age,bribery,weight,height);
+        if (deputy.isBribery()) {
+            deputy.toBribe();
+        }
+        deputyList.add(deputy);
+        System.out.println("Депутат : \n" + deputy.toString() + " Добавлений до Фракції");
+    }
+    public void removeDeputy(){
+        print("Ведіть фамілію депутата ");
+        String surname = scanner.next();
+        print("Ведіть імя депутата");
+        String name = scanner.next();
+
+        Iterator<Deputy> deputyIterator = deputyList.iterator();
+        while (deputyIterator.hasNext()){
+            Deputy nextDeputy = deputyIterator.next();
+            if(nextDeputy.getSurname().equalsIgnoreCase(surname)&&nextDeputy.getName().equalsIgnoreCase(name)){
+                deputyIterator.remove();
+                print("Депутата"+ nextDeputy.toString() + "видалено  з фракції");
+            }else {
+                print("Немає такого депутата ");
+            }
+        }
+    }
+    public void bringOutAllBribersFraction(){
+        for(Deputy deputy : deputyList){
+            if(deputy.isBribery()){
+                print(deputy.toString());
+            }
+        }
+    }
+    public void bringOutBiggestFaction(){
+        int bribeSize = 0;
+        int deputyIndex = -1;
+
+        for (int i = 0; i < deputyList.size(); i++) {
+            if (deputyList.get(i).isBribery()) {
+                if (deputyList.get(i).getBribeSize() > bribeSize) {
+                    bribeSize = deputyList.get(i).getBribeSize();
+                    deputyIndex = deputyList.indexOf(deputyList.get(i));
+                }
+            }
+        }
+
+        if (deputyIndex >= 0) {
+            System.out.println("Самий Великий хабарник :" + deputyList.get(deputyIndex).toString());
+        } else {
+            System.out.println("У фракції немає хабарників !");
+        }
+    }
+    public void allDeputiesFraction(){
+        for(Deputy deputy : deputyList){
+            print(deputy.toString());
+        }
+    }
+    public void removeAllFractionDeputies(){
+        deputyList.clear();
+        print("Всі депутати Видалені ");
+    }
+    @Override
+    public String toString() {
+        return "Фракція \"" + getNameFaction() + "\"";
+    }
+}
